bac6e7d5882f911e7575cfa99c9076479a97dbe4
Mare94 <mn-sand@online.no>
2024-02-20 16:48:18 +0100

    fixed validation

--- src/js/ui/common/validation.js
+++ src/js/ui/common/validation.js
@@ -1,16 +1,12 @@
 const form = document.querySelector("#contactForm");
 const firstName = document.querySelector("#firstName");
 const firstNameError = document.querySelector("#firstNameError");
-const subject = document.querySelector("#subject");
-const subjectError = document.querySelector("#subjectError");
 const message = document.querySelector("#message");
 const messageError = document.querySelector("#messageError");
 const address = document.querySelector("#address");
 const addressError = document.querySelector("#addressError");
 const email = document.querySelector("#email");
 const emailError = document.querySelector("#emailError");
-
-const success = document.querySelector("#success");
 
 // function to run thru and check if len matches or display error msg
 
@@ -22,15 +18,10 @@ function validateForm(event){
     } else{
         firstNameError.style.display = "block";
     }
-    if(checkLength(message.value, 25) === true){
+    if(checkLength(message.value, 10) === true){
         messageError.style.display = "none";
     } else{
         messageError.style.display = "block";
-    }
-    if(checkLength(subject.value, 15) === true){
-        subjectError.style.display = "none";
-    } else{
-        subjectError.style.display = "block";
     }
     if(checkLength(address.value, 5) === true){
         addressError.style.display = "none";
@@ -43,17 +34,16 @@ function validateForm(event){
         emailError.style.display = "block";
     }
 
-    if(checkLength(firstName.value, 0) && checkLength(message.value, 25) && checkLength(subject.value, 15) && checkLength(address.value, 5) && validateEmail(email.value) === true){
+    if(checkLength(firstName.value, 0) && checkLength(message.value, 10) && checkLength(address.value, 5) && validateEmail(email.value) === true){
         success.style.display = "block";
     } else{
         success.style.display = "none";
     }
-    
 
     console.log("it works");
 }
 
-form.addEventListener("submit", validateForm, validateSuccess);
+form.addEventListener("submit", validateForm);
 
 // function to check the length of the passed in value
 
@@ -72,18 +62,4 @@ function validateEmail(email){
     const regEx = /\S+@\S+\.\S+/;
     const patternMatches = regEx.test(email);
     return patternMatches;
-}
-
-function validateSuccess(event){
-
-    event.preventDefault();
-
-    if(validateForm === false){
-        success.style.display = "block";
-        console.log("hey world i work");
-    } else {
-        success.style.display = "block";
-        
-    }
-
 }
