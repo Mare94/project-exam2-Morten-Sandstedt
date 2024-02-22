a7fd6b6cbba1121e891c4067da956abd9f61b88d
Mare94 <mn-sand@online.no>
2024-02-22 17:17:21 +0100

    added apikey in authtoken

--- src/js/api/authToken.js
+++ src/js/api/authToken.js
@@ -1,13 +1,20 @@
 import { load } from "../storage/storage.mjs";
+import { BASE_API_URL } from "./constant";
+
+const path = "/auth/create-api-key";
 
 
 
 export function headers() {
     const token = load("token");
 
+    const apiKey = BASE_API_URL + path;
+
+
     return {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`
+        "Authorization": `Bearer ${token}`,
+        "X-Noroff-API-Key": apiKey
     }
 }
 
