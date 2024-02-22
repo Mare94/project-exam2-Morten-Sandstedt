634cb2cc01df8c460af23ec53cd8d463d7723a3c
Mare94 <mn-sand@online.no>
2024-02-22 17:14:00 +0100

    removed api-key

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -3,19 +3,15 @@ import { BASE_API_URL } from "../constant";
 const path = "/auth/login";
 const method = "post";
 
-const authPath = "/auth/create-api-key";
 
 export async function login(profile){
     const loginUrl = BASE_API_URL + path;
-    const apiKey = BASE_API_URL + authPath;
-
     const body = JSON.stringify(profile);
 
 
     const reponse = await fetch(loginUrl, {
         headers: {
-            "Content-Type": "application/json",
-            "X-Noroff-API-Key": apiKey
+            "Content-Type": "application/json"
         },
         method,
         body
