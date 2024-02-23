a1cdceb4f93b1e75d17d3b92b77f13fcfc1f57ff
Mare94 <mn-sand@online.no>
2024-02-23 20:26:21 +0100

    created booking form listener

---
+++ src/js/handlers/createBookingPost.js
@@ -0,0 +1,21 @@
+import { createPost } from "../api/bookings/index.js";
+
+// handles the creation of posts
+
+export function SetCreateBookingFormListener() {
+    const form = document.querySelector("#createPost");
+
+    form.addEventListener("submit", (event) =>{
+        event.preventDefault()
+        const form = event.target;
+
+        const formData = new FormData(form);
+        const post = Object.fromEntries(formData.entries())
+        
+        //console.log("works!")
+        
+
+        createPost(post)
+
+    })
+};
