61699d2c09512ed3e0fb6ceb03b99b7569d50c7e
Mare94 <mn-sand@online.no>
2024-02-23 18:30:37 +0100

    creatded delete venue

---
+++ src/js/api/venue/deleteVenue.js
@@ -0,0 +1,22 @@
+import { authToken } from "../authToken.js";
+import { BASE_API_URL } from "../constant.js";
+
+const action = "/venues";
+const method = "delete";
+
+export async function removePost(id) {
+
+    if (!id) {
+        throw new Error("Delete requires an ID")
+    }
+
+
+    const removePostUrl = `${BASE_API_URL}${action}/${id}`;
+    
+    const response = await authToken(removePostUrl, {
+        method,
+
+    });
+
+    return await response.JSON();
+}
