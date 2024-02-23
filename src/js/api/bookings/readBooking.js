b5095aa77716e2470abeb4b5f04a5d65784fdb21
Mare94 <mn-sand@online.no>
2024-02-23 20:18:36 +0100

    created read bookings

---
+++ src/js/api/bookings/readBooking.js
@@ -0,0 +1,25 @@
+import { authToken } from "../authToken.js";
+import { BASE_API_URL } from "../constant.js";
+
+const action = "/bookings"
+
+export async function readVenues() {
+
+    const readVenuesUrl = `${BASE_API_URL}${action}`;
+
+    const response = await authToken(readVenuesUrl);
+    return await response.json();
+}
+
+
+export async function readVenue(id) {
+    if(!id){
+        throw new Error("Venue requires an ID")
+    }
+
+    const readVenueUrl = `${BASE_API_URL}${action}/${id}`;
+
+    const reponse = await authToken(readVenueUrl);
+    return await response.json();
+
+}
