b055972e255f6fdb46b40bf1c67ff86e448ebf4f
Mare94 <mn-sand@online.no>
2024-02-23 20:32:06 +0100

    created booking update form listener

---
+++ src/js/handlers/updateBookingPost.js
@@ -0,0 +1,43 @@
+import { readBooking, updateBooking } from "../api/bookings/index.js";
+
+// handles the edit of bookings
+
+export async function SetUpdateVenueFormListener() {
+    const form = document.querySelector("#updatePost");
+
+    const url = new URL(location.href);
+    const id = url.searchParams.get("id");
+
+    if (form) {
+
+        const button = form.querySelector("button");
+        button.disabled = true;
+
+
+        const post = await readBooking(id);
+
+        form.data.dateFrom().value = post.dateForm();
+        form.data.dateTo().value = post.dateTo();
+        form.data.guests.value = post.guests;
+        
+
+        button.disabled = false;
+
+        form.addEventListener("submit", (event) =>{
+            event.preventDefault()
+            const form = event.target;
+    
+            const formData = new FormData(form);
+            const post = Object.fromEntries(formData.entries())
+            post.id = id;
+            
+            console.log("works!")
+            
+    
+            updateBooking(post)
+    
+        })
+
+    }
+    
+};
