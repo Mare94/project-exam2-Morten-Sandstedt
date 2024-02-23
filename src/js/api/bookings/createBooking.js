eef695025c04174d9e5af59b4520cd217ffa1e2d
Mare94 <mn-sand@online.no>
2024-02-23 20:16:09 +0100

    created createbookings

---
+++ src/js/api/bookings/createBooking.js
@@ -0,0 +1,17 @@
+import { authToken } from "../authToken.js";
+import { BASE_API_URL } from "../constant.js";
+
+const action = "/bookings";
+const method = "post";
+
+export async function createPost(postData) {
+    const createPostUrl = BASE_API_URL + action;
+    
+    const response = await authToken(createPostUrl, {
+        method,
+        body: JSON.stringify(postData)
+
+    });
+
+    return await response.JSON();
+};
