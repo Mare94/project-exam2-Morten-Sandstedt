b8828fd60aba045104d3a8db8b163b467b7b225c
Mare94 <mn-sand@online.no>
2024-02-23 18:01:16 +0100

    created update profile form listener

---
+++ src/js/handlers/updateProfile.js
@@ -0,0 +1,48 @@
+import { updateProfile } from "../api/profiles/index.js";
+import { readProfile } from "../api/profiles/index.js";
+import { load } from "../storage/storage";
+
+
+
+// handles the edit of profiles
+
+export async function SetUpdateProfileFormListener() {
+    const form = document.querySelector("#updateProfile");
+
+
+    if (form) {
+
+        const { name, email } = load("profile");
+
+        form.name.value = name;
+        form.email.value = email;
+
+        const button = form.querySelector("button");
+        button.disabled = true;
+
+
+        const profile = await readProfile(name);
+
+        form.banner.value = profile.banner;
+        form.avatar.value = profile.avatar;
+
+        button.disabled = false;
+
+        form.addEventListener("submit", (event) =>{
+            event.preventDefault()
+            const form = event.target;
+    
+            const formData = new FormData(form);
+            const profile = Object.fromEntries(formData.entries())
+            
+            console.log("works!")
+            profile.name = name;
+            profile.email = email;
+    
+            updateProfile(profile)
+    
+        })
+
+    }
+    
+};
