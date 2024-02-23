c0ff245d95f31d85fffce534aebdde9fff686fa3
Mare94 <mn-sand@online.no>
2024-02-23 17:44:17 +0100

    created login form listener

---
+++ src/js/handlers/login.js
@@ -0,0 +1,42 @@
+import { login } from "../api/auth/login";
+import * as storage from "../storage/storage.js";
+import errorMessage from "../ui/common/displayMessage.js";
+
+// handles the login of profiles
+
+export function SetLoginFormListener(){
+    const form = document.querySelector("#loginForm");
+
+    form.addEventListener("submit", async (event) =>{
+        event.preventDefault()
+        const form = event.target;
+
+        const formData = new FormData(form);
+        const profile = Object.fromEntries(formData.entries())
+        // console.log("works!")
+
+        const button = form.querySelector("button");
+        button.innerText = "Logging in...";
+        const fieldset = form.querySelector("fieldset");
+        fieldset.disabled = true;
+        
+        try {
+            const { accessToken, ...user } = await login(profile)
+            
+
+            storage.save("token", accessToken);
+            storage.save("profile", user);
+            location.href = "/profile/"
+
+        } 
+        catch(error) {
+            errorMessage("danger", error, "#message")
+        }
+        finally {
+            button.innerText = "Login";
+            fieldset.disabled = false;
+        }
+        
+
+    })
+};
