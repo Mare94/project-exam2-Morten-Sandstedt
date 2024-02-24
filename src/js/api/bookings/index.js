37bc0f3e7cf69069d9ad0ef29018da1ba0a190df
Mare94 <mn-sand@online.no>
2024-02-24 21:33:38 +0100

    changing to mjs

--- src/js/api/bookings/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./createBooking.js";
-export * from "./readBooking.js";
-export * from "./updateBooking.js";
-export * from "./deleteBooking.js";
---
+++ src/js/api/bookings/index.mjs
@@ -0,0 +1,4 @@
+export * from "./createBooking.mjs";
+export * from "./readBooking.mjs";
+export * from "./updateBooking.mjs";
+export * from "./deleteBooking.mjs";
