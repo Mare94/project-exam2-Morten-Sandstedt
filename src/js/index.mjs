6fc506cb4226e97fa2d2873d3cded479665e53a7
Mare94 <mn-sand@online.no>
2024-02-25 16:28:43 +0100

    created index pathing js

---
+++ src/js/index.mjs
@@ -0,0 +1,23 @@
+import * as listeners from "./handlers/index.mjs";
+import * as venueAction from "./api/venue/index.mjs";
+import * as bookingAction from "./api/bookings/index.mjs";
+
+import * as SetUpdateVenueFormListener from "./handlers/updateBookingPost.mjs";
+
+const path = location.pathname;
+
+if(path === '/profile/login/') {
+    listeners.SetLoginFormListener()
+} else if(path === '/profile/register/') {
+    listeners.SetRegisterFormListener()
+} else if(path === '/product/create/'){
+    listeners.SetCreateBookingFormListener()
+} else if(path === '/product/create/') {
+    listeners.SetCreateVenueFormListener
+} else if(path === '/product/edit/') {
+    listeners.SetUpdateVenueFormListener()
+} else if(path === '/product/edit/') {
+    listeners.SetUpdateVenueFormListener()
+} else if(path === '/profile/edit/'){
+    listeners.SetUpdateProfileFormListener()
+}
